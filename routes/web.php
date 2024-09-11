bf9cab5d338bb21375973bb30c1519b3fc34173e
Tegar <tegaradityansyah07@gmail.com>
2024-09-11 08:06:44 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,17 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 Route::post('/login', [UserController::class, 'action'])->name('login');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
 
 //admin
 Route::get('/admin', [AdminController::class, 'index'])->name('dashboard.admin');
