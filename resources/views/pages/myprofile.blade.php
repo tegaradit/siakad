29df9a2f9849317f80edff1c6f7c8550f7b32b0a
Tegar <tegaradityansyah07@gmail.com>
2024-09-12 09:41:56 +0700

    new login

--- resources/views/pages/myprofile.blade.php
+++ resources/views/pages/myprofile.blade.php
@@ -1,28 +1,6 @@
+@extends('layouts.home-layout')
 
 @section('home-content')
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <title>Profile | Minia - Minimal Admin & Dashboard Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('minia/assets/images/favicon.ico') }}">
-
-    <!-- preloader css -->
-    <link rel="stylesheet" href="{{ asset('minia/assets/css/preloader.min.css') }}" type="text/css" />
-    <!-- Bootstrap Css -->
-    <link href="{{ asset('minia/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{ asset('minia/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href="{{ asset('minia/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
     <div class="main-content">
         <div class="page-content">
             <div class="container-fluid">
@@ -138,17 +116,4 @@
             </div>
         </div>
     </div>
-
-    <!-- JAVASCRIPT -->
-    <script src="{{ asset('minia/assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/libs/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/libs/pace-js/pace.min.js') }}"></script>
-    <script src="{{ asset('minia/assets/js/app.js') }}"></script>
-</body>
-
-</html>
 @endsection
