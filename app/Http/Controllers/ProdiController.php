1b7c002262f342e3d9df3234c6b56edc06908a08
putri <putrinurinutasari08@gmail.com>
2024-09-12 11:46:12 +0700

    tabel prodi

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\DataTables;
 
 class ProdiController extends Controller
