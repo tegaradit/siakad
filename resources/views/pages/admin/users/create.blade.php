a1bcc17cc848c93168e24f331f84854095dee340
Tegar <tegaradityansyah07@gmail.com>
2024-09-17 14:24:53 +0700

    ghghgjsdjksh

--- resources/views/pages/admin/users/create.blade.php
+++ resources/views/pages/admin/users/create.blade.php
@@ -1,49 +1,61 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Tambah User</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
+@extends('layouts.home-layout')
+
+@section('home-content')
+
 <body>
-    <div class="container">
-        <h1>Tambah User</h1>
-        <form action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="name">Nama</label>
-                <input type="text" name="name" class="form-control" required>
+    <div class="main-content">
+        <div class="page-content">
+            <div class="container-fluid">
+                <div class="wrapper">
+                    <header class="header">
+                        <div class="container">
+                            <h1 class="header-title">Tambah User</h1>
+                        </div>
+                    </header>
+
+                    <main class="container">
+                        <form action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data" class="form">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <label for="name">Nama</label>
+                                <input type="text" name="name" id="name" class="form-control" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="phone_number">Nomor Telepon</label>
+                                <input type="text" name="phone_number" id="phone_number" class="form-control" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" id="email" class="form-control" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" class="form-control" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="password_confirmation">Konfirmasi Password</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="role_id">Role</label>
+                                <select name="role_id" id="role_id" class="form-control" required>
+                                    @foreach($roles as $role)
+                                    <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="photo">Foto</label>
+                                <input type="file" name="photo" id="photo" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary me-2">Simpan</button>
+                                <a href="{{ route('users.index') }}" class="btn btn-secondary">Batal</a>
+                            </div>
+                        </form>
+                    </main>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="phone_number">Nomor Telepon</label>
-                <input type="text" name="phone_number" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" name="email" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name="password" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="password_confirmation">Konfirmasi Password</label>
-                <input type="password" name="password_confirmation" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="role_id">Role</label>
-                <select name="role_id" class="form-control" required>
-                    @foreach($roles as $role)
-                        <option value="{{ $role->id }}">{{ $role->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="photo">Foto</label>
-                <input type="file" name="photo" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-success">Simpan</button>
-            <a href="{{ route('users.index') }}" class="btn btn-secondary">Batal</a>
-        </form>
-    </div>
-</body>
-</html>
+        </div>
+        <script src="{{ asset('minia/js/minia.min.js') }}"></script>
+        @endsection
