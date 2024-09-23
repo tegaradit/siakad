2285d5b1de9490c0b153095500beda6b79ee9757
maulana adam <wandgenius6@gmail.com>
2024-09-23 08:31:32 +0700

    Refactor ProdiDataTable and Prodi model
    - Remove unused code and commented out sections in ProdiDataTable.php
    - Remove Prodi model and its migration file
    
    Closes #123

---
+++ database/seeders/IdentitasPt.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IdentitasPt extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\IdentitasPt::insert([
+            'currrent_npsn' => 053025,
+            'current_id_sp' => '4d31c6cd-2e8e-4144-b954-cb95b6ebd5ce'
+        ]);
+    }
+}
