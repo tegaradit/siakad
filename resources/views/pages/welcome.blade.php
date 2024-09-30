db09a31e42491782f83e059898cf7f28954fa740
Tegar <tegaradityansyah07@gmail.com>
2024-09-30 10:40:33 +0700

    new migrations

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -85,34 +85,10 @@
 
                   <div class="mt-4 pt-2 text-center">
                     <div class="signin-other-title">
-                      <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign in with -</h5>
+                      <h5 class="font-size-14 mb-3 text-muted fw-medium"></h5>
                     </div>
 
-                    <ul class="list-inline mb-0">
-                      <li class="list-inline-item">
-                        <a href="javascript:void()"
-                          class="social-list-item bg-primary text-white border-primary">
-                          <i class="mdi mdi-facebook"></i>
-                        </a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="javascript:void()"
-                          class="social-list-item bg-info text-white border-info">
-                          <i class="mdi mdi-twitter"></i>
-                        </a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="javascript:void()"
-                          class="social-list-item bg-danger text-white border-danger">
-                          <i class="mdi mdi-google"></i>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-
-                  <div class="mt-5 text-center">
-                    <p class="text-muted mb-0">Don't have an account ? <a href="auth-register.html"
-                        class="text-primary fw-semibold"> Signup now </a> </p>
+                    
                   </div>
                 </div>
                 <div class="mt-4 mt-md-5 text-center">
